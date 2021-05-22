6528583f69bd8066ef15c0825c396da02230739d
gywlsp <gywls517@gmail.com>
2021-05-23 00:52:41 +0900

    feat: :sparkles: add template data fetch swr hooks
    useTemplateList, useTemplate

---
+++ src/hooks/api/template.ts
@@ -0,0 +1,22 @@
+import useSWR from 'swr';
+import axios, { AxiosResponse, AxiosError } from 'axios';
+
+import { TemplateType } from 'src/types/template';
+
+const fetcher = (url) => axios.get(url);
+
+export const useTemplateList = () => {
+  const { data: response, error } = useSWR<
+    AxiosResponse<{ result: TemplateType[] }>,
+    AxiosError<Error>
+  >(`${process.env.API_HOST}/templates`, fetcher);
+  return { data: response?.data, error };
+};
+
+export const useTemplate = (id: number) => {
+  const { data: response, error } = useSWR<
+    AxiosResponse<{ result: TemplateType }>,
+    AxiosError<Error>
+  >(`${process.env.API_HOST}/templates/${id}`, fetcher);
+  return { data: response?.data, error };
+};
