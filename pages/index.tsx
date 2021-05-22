3fd87d10db546be8a2ec536df9652794bbfdd8d8
gywlsp <gywls517@gmail.com>
2021-05-22 20:02:50 +0900

    feat: :sparkles: add GlobalHeader, Sidebar to Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { ReactNode, ReactNodeArray } from 'react';
+import styled from 'styled-components';
+
+import GlobalHeader from 'src/components/header';
+import Sidebar from 'src/components/sidebar';
+import { WHITE } from 'src/constants/colors';
+
+export type GlobalLayoutProps = {
+  children: ReactNode | ReactNodeArray;
+};
 
 export default function Home() {
-  return <></>;
+  return (
+    <Wrapper>
+      <GlobalHeader />
+      <Content>
+        <Sidebar />
+      </Content>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  background-color: ${WHITE};
+`;
+
+const Content = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+`;
