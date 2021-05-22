f0f4151e99b6e0142c9ceb205db27c623a9817da
gywlsp <gywls517@gmail.com>
2021-05-22 20:42:09 +0900

    chore: :lipstick: change Sidebar height

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 
 import ExcelInputSection from './section/excel-input';
+import RecipientListSection from './section/recipient-list';
 import { GREY } from 'src/constants/colors';
 
 export default function Sidebar() {
   return (
     <Wrapper>
       <ExcelInputSection />
+      <RecipientListSection />
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
   width: 34.5rem;
-  height: 100vh;
+  height: calc(100vh - 6rem);
   background-color: ${GREY[50]};
 `;
