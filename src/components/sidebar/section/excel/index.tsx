268f46d0f0f2f760562dab8824fb1de21aa78f40
chorom-ham <chorom.m.ham@gmail.com>
2021-05-23 10:44:56 +0900

    feat: ✨ add excel download button

--- src/components/sidebar/section/excel/index.tsx
+++ src/components/sidebar/section/excel/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { GREY } from 'src/constants/colors';
 import styled from 'styled-components';
 
+import DownloadExcelButton from './download-excel';
 import NewExcelFileInput from './new-input';
 import RecentExcelFileSelect from './recent-select';
 
 export default function ExcelInputSection() {
   return (
     <Wrapper>
+      <DownloadExcelButton />
       <NewExcelFileInput />
       <RecentExcelFileSelect />
     </Wrapper>
